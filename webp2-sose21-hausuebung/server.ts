b1615b5dd171b23970f84ec02448ab076acdaf6d
Busfreak <martin@middeke.de>
2021-05-11 00:08:19 +0200

    CRUD für User

--- webp2-sose21-hausuebung/server.ts
+++ webp2-sose21-hausuebung/server.ts
@@ -1,13 +1,25 @@
 import * as express from "express";
-
 const PORT: number = 8080;
-
 const router: express.Express = express();
+
+class User {
+    public readonly username: string;
+    public vorname: string;
+    public nachname: string;
+    private passwort: string;
+
+    constructor(username: string, vorname: string, nachname: string, passwort: string) {
+        this.username = username;
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.passwort = passwort;
+    }
+}
+const users: Map<string, User> = new Map<string, User>();
 
 router.listen (PORT, () => {
     console.log("Server ist gestartet unter http://localhost:" + PORT + "/");
 });
-
 
 router.use(express.urlencoded({extended: false}));
 router.use(express.json());
@@ -22,4 +34,40 @@ router.get("/", (reg: express.Request, res: express.Response) => {
 router.use("/res", express.static(__dirname + "/client"));
 router.use("/lib", express.static(__dirname + "/node_modules"));
 
-console.log("Server is an")
+// Routen für REST
+router.post("/user", postUser);
+router.get("/user/:username", getUser);
+router.delete("/user/:username", deleteUser)
+router.put("/user/:username", updateUser)
+
+
+// Funktionen für REST
+function postUser(req: express.Request, res: express.Response): void {
+    const username: string = req.body.username;
+    const vorname: string = req.body.vorname;
+    const nachname: string = req.body.nachname;
+    const passwort: string = req.body.passwort;
+    if (!users.has(username)) {
+        users.set(username, new User(username, vorname, nachname, passwort));
+//        res.send("Der Benutzer " + vorname + " " + nachname + " wurde angelegt");
+        res.sendStatus(201);
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+function getUser(req: express.Request, res: express.Response): void {
+    const username: string = req.params.username;
+    res.send("Der User mit dem Usernamen " + username + " existiert :)");
+}
+
+function deleteUser(req: express.Request, res: express.Response): void {
+    const username: string = req.params.username;
+    res.send("Der User mit dem Usernamen " + username + " wurde gelöscht :)");
+}
+
+function updateUser(req: express.Request, res: express.Response): void {
+    const username: string = req.params.username;
+    res.send("Der User mit dem Usernamen " + username + " wurde aktualisiert :)");
+}
+
