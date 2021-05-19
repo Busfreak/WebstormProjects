7dec885808b17ffb6b2c149de9f7eb9fbfc62087
Busfreak <martin@middeke.de>
2021-05-19 03:12:57 +0200

    Usermanagement umgesetzt, bis auf Löschen

--- webp2-sose21-hausuebung/client/script.ts
+++ webp2-sose21-hausuebung/client/script.ts
@@ -111,7 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // Login/Register/Logout Buttons rendern
 function renderUserInfo(): void {
     //prüfen, ob ein User angemeldet ist, oder nicht
-    // AJAX Request: alle User aus Backend abfragen
+    // AJAX Request: ist ein Benutzer angemeldet, dann empfange die Daten des Benutzer
     axios.get("/loggedin").then((res: AxiosResponse) => {
         const userinfo: HTMLDivElement = document.getElementById("userinfo") as HTMLDivElement;
         userinfo.innerHTML = "angemeldet als: " + res.data["vorname"] + " " + res.data["nachname"] + `
@@ -234,8 +234,8 @@ function login():void{
     const out: HTMLElement = document.getElementById("out");
     const loginform: HTMLFormElement = document.getElementById("loginform") as HTMLFormElement;
     const data: FormData = new FormData(loginform);
-    const username: string = data.get("loginName");
-    const passwort: string = data.get("loginPasswort");
+    const username: string = String(data.get("loginName"));
+    const passwort: string = String(data.get("loginPasswort"));
     axios.post("/login", {
         username: username,
         passwort: passwort
@@ -401,6 +401,7 @@ function renderRegistrationForm():void {
 // Userliste erstellen und anzeigen
 function renderUserlist(): void {
     axios.get("/loggedin").then((res: AxiosResponse) => {
+        const username: string = res.data["username"];
         // Überschriften erstellen und anzeigen
         let thead: HTMLElement = document.getElementById("user-thead");
         thead.innerHTML = "";
@@ -415,39 +416,33 @@ function renderUserlist(): void {
         let table: HTMLElement = document.getElementById("user-table");
         table.innerHTML = "";
 
-        // AJAX Request: alle User aus Backend abfragen
-        axios.get("/users").then((res: AxiosResponse) => {
-            // Zähler für data-index, damit die angeklickte Zeile später gefunden werden kann - kann das auch durch den username ersetzt werden?
-            let i: number = 0;
-            // die einzelnen Datensätze aus der Response durchgehen und in die Tabelle einfügen
-            for (let value in res.data) {
-                const row: HTMLElement = document.createElement("tr");
-                // Zeilennummer setzen
-                row.setAttribute("id", "row" + i);
-                row.innerHTML = `
-                <td class="col-3">${res.data[value]["username"]}</td>
-                <td class="col-3">${res.data[value]["vorname"]}</td>
-                <td class="col-3">${res.data[value]["nachname"]}</td>
-                <td class="col-3" id="buttonliste">
-                   <button type="submit" class="edit-button btn btn-outline-primary btn-sm line-darkred text-red" data-username=${res.data[value]["username"]} data-index="${i}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                        </svg>
-                    Editieren</button>
-                    <button type="submit" class="delete-button btn btn-outline-primary btn-sm line-darkred text-red" data-username=${res.data[value]["username"]} data-index="${i}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                        </svg>
-                    Löschen</button>
-                    <button type="submit" class="password-button btn btn-outline-primary btn-sm line-darkred text-red" data-username=${res.data[value]["username"]}" data-index="${i}">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
-                        <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
-                        </svg>
-                    Passwort ändern</button>
-                </td>`;
-                table.appendChild(row);
-                i++;
-            }
+        // AJAX Request: eigene Userdaten aus Backend abfragen
+        axios.get("/user/" + username).then((res: AxiosResponse) => {
+            const row: HTMLElement = document.createElement("tr");
+            // Zeilennummer setzen
+            row.setAttribute("id", "row1");
+            row.innerHTML = `
+            <td class="col-3">${username}</td>
+            <td class="col-3">${res.data["vorname"]}</td>
+            <td class="col-3">${res.data["nachname"]}</td>
+            <td class="col-3" id="buttonliste">
+               <button type="submit" class="edit-button btn btn-outline-primary btn-sm line-darkred text-red" data-username=${res.data["username"]} data-index="1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                    </svg>
+                Editieren</button>
+                <button type="submit" class="delete-button btn btn-outline-primary btn-sm line-darkred text-red" data-username=${res.data["username"]} data-index="1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg>
+                Löschen</button>
+                <button type="submit" class="password-button btn btn-outline-primary btn-sm line-darkred text-red" data-username=${res.data["username"]}" data-index="1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                    <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                    </svg>
+                Passwort ändern</button>
+            </td>`;
+            table.appendChild(row);
         }).catch((err: AxiosError) => {
             if(err.response.status == 204) {
                 console.log("Keine Benutzer gefunden.");
--- webp2-sose21-hausuebung/server.ts
+++ webp2-sose21-hausuebung/server.ts
@@ -98,7 +98,6 @@ router.post("/user", postUser);
 router.get("/user/:username", checkLogin, getUser);
 router.delete("/user/:username", checkLogin, deleteUser);
 router.put("/user/:username", checkLogin, updateUser);
-router.get("/users", checkLogin, getUsers);
 router.post("/savepassword", checkLogin, savePassword);
 router.get("/loggedin", loggedIn);
 
@@ -109,19 +108,19 @@ router.put("/pet/:tiername", checkLogin, updatePet);
 
 // Autorisiert die Session, falls Benutzer registriert ist
 function login(req: express.Request, res: express.Response): void {
-    const username: string = req.body.username;
-    const passwort: string = req.body.passwort;
-    if (users.has(username)) {
-        const u: User = users.get(username);
-        if (u.getPasswort === passwort) {
-            req.session.uname = username
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(403);
-        }
-    } else {
-        res.sendStatus(404);
-    }
+    query("SELECT null FROM user WHERE username = ? AND passwort = ?;", [req.body.username, req.body.passwort])
+        .then((results)=>{
+            if(results.length == 1) {
+                req.session.uname = req.body.username;
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch((err)=>{
+            console.log("LOGIN", err);
+            res.sendStatus(404);
+        })
 }
 
 // Beendet die Session
@@ -136,11 +135,19 @@ function logout(req: express.Request, res: express.Response): void {
 // Prüfen, ob ein User angemeldet ist
 function loggedIn(req: express.Request, res: express.Response): void {
     if (req.session.uname !== undefined) {
-        if (users.has(req.session.uname)) {
-            const u: User = users.get(req.session.uname);
-            res.status(200);
-            res.json({"username": u.username, "vorname": u.vorname, "nachname": u.nachname});
-        }
+        query("SELECT username, vorname, nachname FROM user WHERE username = ?;", [req.session.uname])
+            .then((results)=>{
+                if(results.length == 1) {
+                    res.status(200);
+                    res.json({"username": results[0]["username"], "vorname": results[0]["vorname"], "nachname": results[0]["nachname"]});
+                } else {
+                    res.sendStatus(404);
+                }
+            })
+            .catch((err)=>{
+                console.log("LOGIN", err);
+                res.sendStatus(500);
+            })
     } else {
         // Client nicht angemeldet
         res.sendStatus(401);
@@ -165,91 +172,112 @@ function postUser(req: express.Request, res: express.Response): void {
     const vorname: string = req.body.vorname;
     const nachname: string = req.body.nachname;
     const passwort: string = req.body.passwort;
-    if (!users.has(username)) {
-        users.set(username, new User(username, vorname, nachname, passwort));
-//        res.send("Der Benutzer " + vorname + " " + nachname + " wurde angelegt");
-        res.sendStatus(201);
-    } else {
-        res.sendStatus(403);
-    }
+    query("SELECT NULL FROM user WHERE username = ?;", [req.session.uname])
+        .then((results)=>{
+            if(results.length == 0) {
+                query("INSERT INTO user (username, vorname, nachname, passwort) VALUES ('?', '?', '?', '?');", [req.body.username, req.body.vorname, req.body.nachname, req.body.passwort])
+                    .then((results)=>{
+                        if(results.length == 1) {
+                            res.sendStatus(200);
+                        } else {
+                            res.sendStatus(404);
+                        }
+                    })
+                    .catch((err)=>{
+                        console.log("postUser", err);
+                        res.sendStatus(500);
+                    })
+//                res.status(201);
+//                res.json({"username": res.json(results[0]), "vorname": res.json(results[1]), "nachname": res.json(results[2])});
+            } else {
+                res.sendStatus(403);
+            }
+        })
+        .catch((err)=>{
+            console.log("LOGIN", err);
+            res.sendStatus(500);
+        })
 }
 
 // User auslesen und zurück senden
 function getUser(req: express.Request, res: express.Response): void {
-    const username: string = req.params.username;
-//    res.send("Der User mit dem Usernamen " + username + " existiert :)");
-    if (users.has(username)) {
-        const u: User = users.get(username);
-        res.status(200);
-        res.json({"vorname":u.vorname,"nachname":u.nachname});
-    } else {
-        res.sendStatus(404);
-    }
+    query("SELECT vorname, nachname FROM user WHERE username = ?;", [req.params.username])
+        .then((results)=>{
+            if(results.length == 1) {
+                res.status(200);
+                res.json({"username": req.params.username, "vorname": results[0]["vorname"], "nachname": results[0]["nachname"]});
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch((err)=>{
+            console.log("getUser", err);
+            res.sendStatus(500);
+        })
 }
 
 // User löschen
 function deleteUser(req: express.Request, res: express.Response): void {
-    const username: string = req.params.username;
-//    res.send("Der User mit dem Usernamen " + username + " wurde gelöscht :)");
-    if (req.session.uname != username) {
-        if (users.has(username)) {
-            users.delete(username);
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
-    } else {
-        res.sendStatus(403);
-    }
+    query("DELETE FROM user WHERE username = ?;", [req.params.username])
+        .then((results)=>{
+            if(results.length == 1) {
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch((err)=>{
+            console.log("deleteUSer", err);
+            res.sendStatus(500);
+        })
 }
 
 // User aktualisieren
 function updateUser(req: express.Request, res: express.Response): void {
-    const username: string = req.body.username;
-    const vorname: string = req.body.vorname;
-    const nachname: string = req.body.nachname;
-    if (users.has(username)) {
-        const u: User = users.get(username);
-        if (vorname.length > 0) {u.vorname = vorname}
-        if (nachname.length  > 0) {u.nachname = nachname}
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(404);
-    }
-}
-
-// alle User auslesen und zurück senden
-function getUsers(req: express.Request, res: express.Response): void {
-// https://www.cloudhadoop.com/2018/09/typescript-how-to-convert-map-tofrom.html
-    if (users.size > 0) {
-        let jsonObject = {};
-        users.forEach((value, key) => {
-            jsonObject[key] = value.userInfo;
-        });
-        res.status(200);
-        res.json(jsonObject);
-    } else {
-        res.sendStatus(204);
-    }
+    query("UPDATE user SET vorname = ?, nachname = ? WHERE username = ?;", [req.body.vorname, req.body.nachname, req.body.username])
+        .then((results)=>{
+            if(results.affectedRows == 1) {
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch((err)=>{
+            console.log("LOGIN", err);
+            res.sendStatus(500);
+        })
 }
 
 // Passwort prüfen und speichern
 function savePassword(req: express.Request, res: express.Response): void {
-    const username: string = req.body.username;
-    const oldPassword: string = req.body.old;
-    const newPassword: string = req.body.new;
-    if (users.has(username)) {
-        const u: User = users.get(username);
-        if(oldPassword != u.getPasswort) {
-            // Anfrage nicht erlaubt - "Das alte Passwort ist nicht korrekt!"
-            res.sendStatus(403);
-        } else {
-            u.setPasswort = newPassword;
-            res.sendStatus(200);
+    query("SELECT passwort FROM user WHERE username = ?;", [req.body.username])
+        .then((results)=>{
+            if(results.length == 1) {
+                if(req.body.old != results[0]["passwort"]) {
+                    // Anfrage nicht erlaubt - "Das alte Passwort ist nicht korrekt!"
+                    res.sendStatus(403);
+                } else {
+                    query("UPDATE user SET passwort = ? WHERE username = ?;", [req.body.new, req.body.username])
+                        .then((results)=>{
+                            if(results.affectedRows == 1) {
+                                res.sendStatus(200);
+                            } else {
+                                res.sendStatus(400);
+                            }
+                        })
+                        .catch((err)=>{
+                            console.log("savePassword", err);
+                            res.sendStatus(500);
+                        })
+                }
+            } else {
+                res.sendStatus(400);
             }
-        } else {
-        res.sendStatus(401);
-    }
+        })
+        .catch((err)=>{
+            console.log("savePassword", err);
+            res.sendStatus(500);
+        })
 }
 
 // neues Tier anlegen
